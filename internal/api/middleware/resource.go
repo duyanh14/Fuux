01fe5775be505ccb98b13486ab266327f16fa2e5
DinhTienDat <handestd@gmail.com>
2023-12-21 09:48:10 +0700

    fix: disable old code

--- internal/api/middleware/resource.go
+++ internal/api/middleware/resource.go
@@ -4,10 +4,10 @@ import "github.com/gofiber/fiber/v2"
 
 func resource() func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
-		err := _auth(c)
-		if err != nil {
-			return c.JSON(entity.ResponseError(err))
-		}
+		//err := _auth(c)
+		//if err != nil {
+		//	return c.JSON(entity.ResponseError(err))
+		//}
 
 		return c.Next()
 	}
