d5702e4d74b4ec137f05df67fd9c614931915b1c
DinhTienDat <handestd@gmail.com>
2023-12-22 14:12:55 +0700

    feat: add some validate

--- internal/api/handler/resource/path.go
+++ internal/api/handler/resource/path.go
@@ -7,6 +7,7 @@ import (
 	"fuux/pkg"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	"path/filepath"
 )
 
 type pathHandler struct {
@@ -49,24 +50,27 @@ func (h *pathHandler) create(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"error": "missing required parameter"})
 	}
 
-	var path = entity.Path{
+	name := data["name"]
+	path := filepath.Dir(data["path"])
+
+	var pathModel = entity.Path{
 		ID:   uuid.NewString(),
-		Name: data["name"],
-		Path: data["path"],
+		Name: name,
+		Path: path,
 	}
 
-	pathExist, _ := repository.MatchRecord("path", data["path"], &entity.Path{})
+	pathExist, _ := repository.MatchRecord("path", path, &entity.Path{})
 	if pathExist == true {
 		return c.JSON(fiber.Map{"error": "path already exists"})
 	}
 
-	result := repository.File.Db.Model(&entity.Path{}).Create(&path)
+	result := repository.File.Db.Model(&entity.Path{}).Create(&pathModel)
 
 	if result.Error != nil {
 		return c.JSON(fiber.Map{"error": result.Error.Error()})
 	}
 
-	return c.JSON(path)
+	return c.JSON(pathModel)
 
 }
 func (h *pathHandler) update(c *fiber.Ctx) error {
@@ -88,12 +92,16 @@ func (h *pathHandler) update(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"error": "path does not exists"})
 	}
 	if rawPath == nil {
-		return c.JSON(fiber.Map{"error": "can not found your user"})
+		return c.JSON(fiber.Map{"error": "can not found your path"})
 	}
 
-	path := rawPath.(*entity.Path)
+	pathModel := rawPath.(*entity.Path)
 
-	rs := repository.File.Db.Model(&path).Updates(data)
+	if pathModel.ID != data["id"].(string) && pathModel.Path == data["path"].(string) {
+		return c.JSON(fiber.Map{"error": "this path exist in database"})
+	}
+
+	rs := repository.File.Db.Model(&pathModel).Updates(data)
 	if rs.Error != nil {
 		return c.JSON(fiber.Map{"error": rs.Error.Error()})
 	}
